94b603de6799c8357132d7a1e32d42d378f9793a
Walker Erekson <60948031+texasRanger09@users.noreply.github.com>
2023-10-17 14:58:17 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using DittoSDK;
+using System.Collections.Generic;
+
+namespace Tasks
+{
+    class Program
+    {
+        static Ditto ditto;
+        static bool isAskedToExit = false;
+        static List<Task> tasks = new List<Task>();
+        static DittoSubscription subscription;
+        static DittoLiveQuery liveQuery;
+
+
+        public static void Main(params string[] args)
+        {
+            ditto = new Ditto(identity: DittoIdentity.OnlinePlayground("REPLACE_ME_WITH_YOUR_APP_ID", "REPLACE_ME_WITH_YOUR_PLAYGROUND_TOKEN"));
+
+            try
+            {
+                ditto.StartSync();
+            }
+            catch (DittoException ex)
+            {
+                Console.WriteLine("There was an error starting Ditto.");
+                Console.WriteLine("Here's the following error");
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Ditto cannot start sync but don't worry.");
+                Console.WriteLine("Ditto will still work as a local database.");
+            }
+
+            Console.WriteLine("Welcome to Ditto's Task App");
+
+            subscription = ditto.Store["tasks"].Find("!isDeleted").Subscribe();
+            liveQuery = ditto.Store["tasks"].Find("!isDeleted").ObserveLocal((docs, _event) => {
+                tasks = docs.ConvertAll(document => new Task(document));
+            });
+
+            ditto.Store["tasks"].Find("isDeleted == true").Evict();
+
+            ListCommands();
+
+            while (!isAskedToExit)
+            {
+                
+                Console.Write("\nYour command: ");
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    
+                    case string s when command.StartsWith("--upsert"):
+                        string taskBody = s.Replace("--upsert ", "");
+                        ditto.Store["tasks"].Upsert(new Task(taskBody, false).ToDictionary());
+                        break;
+                    case string s when command.StartsWith("--toggle"):
+                        string _idToToggle = s.Replace("--toggle ", "");
+                        ditto.Store["tasks"]
+                            .FindById(new DittoDocumentId(_idToToggle))
+                            .Update((mutableDoc) => {
+                                if (mutableDoc == null) return;
+                                mutableDoc["isCompleted"].Set(!mutableDoc["isCompleted"].BooleanValue);
+                            });
+                        break;
+                    case string s when command.StartsWith("--delete"):
+                        string _idToDelete = s.Replace("--delete ", "");
+                        ditto.Store["tasks"]
+                            .FindById(new DittoDocumentId(_idToDelete))
+                            .Update((mutableDoc) => {
+                                if (mutableDoc == null) return;
+                                mutableDoc["isDeleted"].Set(true);
+                            });
+                        break;
+                    case { } when command.StartsWith("--list"):
+                        tasks.ForEach(task =>
+                        {
+                            Console.WriteLine(task.ToString());
+                        });
+                        break;
+                    case { } when command.StartsWith("--exit"):
+                        Console.WriteLine("Good bye!");
+                        isAskedToExit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command");
+                        ListCommands();
+                        break;
+                }
+            }
+        }
+
+        public static void ListCommands()
+        {
+            Console.WriteLine("************* Commands *************");
+            Console.WriteLine("--upsert my new task");
+            Console.WriteLine("   Upsert a task");
+            Console.WriteLine("   Example: \"--upsert Get Milk\"");
+            Console.WriteLine("--toggle myTaskTd");
+            Console.WriteLine("   Toggles the isComplete property to the opposite value");
+            Console.WriteLine("   Example: \"--toggle 1234abc\"");
+            Console.WriteLine("--delete myTaskTd");
+            Console.WriteLine("   Deletes a task");
+            Console.WriteLine("   Example: \"--delete 1234abc\"");
+            Console.WriteLine("--list");
+            Console.WriteLine("   List the current tasks");
+            Console.WriteLine("--exit");
+            Console.WriteLine("   Exits the program");
+            Console.WriteLine("************* Commands *************");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # template-app-csharp-todo
+
+Read the full documentation on [ditto.live](https://docs.ditto.live/csharp-installing)
+
+1. Copy your Ditto AppID and token from the [Ditto Portal](https://portal.ditto.live/) 
+to `Program.cs`
+2. This is a terminal application. To run, cd to the root and then `dotnet run`.
